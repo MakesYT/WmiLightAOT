a1c8f7117114ce9f5d0562e108b3dc9ec86931db
MakesYT <2696703792@qq.com>
2024-09-16 23:28:04 +0800

    1

---
+++ WmiLightAOT/Program.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia;
+using System;
+using WmiLight;
+
+namespace WmiLightAOT;
+
+sealed class Program
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        using var wmiConnection = new WmiConnection("root\\cimv2");
+
+        var mos = wmiConnection.CreateQuery(
+            "SELECT Name, NumberOfCores, SocketDesignation FROM Win32_Processor");
+        var firstLine = true;
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
+}
---
+++ WmiLightAOT/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WmiLightAOT.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
